b0969fe568f484ac5fc0492d667cf6606bee3d8b
TaeTaehoon <rad572985@gmail.com>
2022-06-26 16:19:11 +0900

    무한 스크롤링 문제 해결...야매?로 ㅜㅜ

--- pageScroll.js
+++ pageScroll.js
@@ -1,5 +1,7 @@
 const scrollBtns = document.querySelectorAll("#scroll-list a");
-
+const bgImgs = document.querySelectorAll("#container img");
+const footer = document.querySelector("#page-footer");
+let focusIndex = 0;
 scrollBtns.forEach((element) => {
   const target = element.children[0];
   element.addEventListener("click", () => {
@@ -7,5 +9,50 @@ scrollBtns.forEach((element) => {
       btns.children[0].classList.remove("on-focus");
     });
     target.classList.add("on-focus");
+    focusIndex = parseInt(element.id);
   });
+});
+// function trackingIndex() {}
+
+function scrolling(e) {
+  console.log("wheel!");
+
+  const presentView = focusIndex;
+  if (e.clientY > 158.5) {
+    if (e.deltaY < -8.5 && focusIndex > 0) {
+      bgImgs[presentView - 1].scrollIntoView({ behavior: "smooth" });
+      focusIndex = presentView - 1;
+      scrollBtns.forEach((btns) => {
+        btns.children[0].classList.remove("on-focus");
+      });
+      scrollBtns[focusIndex].children[0].classList.add("on-focus");
+      pauseScroll();
+    } else if (e.deltaY > 8.5 && focusIndex < 3) {
+      if (presentView === 2) {
+        footer.scrollIntoView({ behavior: "smooth" });
+      } else {
+        bgImgs[presentView + 1].scrollIntoView({ behavior: "smooth" });
+      }
+
+      focusIndex = presentView + 1;
+      scrollBtns.forEach((btns) => {
+        btns.children[0].classList.remove("on-focus");
+      });
+      scrollBtns[focusIndex].children[0].classList.add("on-focus");
+      pauseScroll();
+    }
+  }
+}
+
+function pauseScroll() {
+  document.body.removeEventListener("wheel", scrolling);
+  setTimeout(addScroll, 750);
+}
+function addScroll() {
+  document.body.addEventListener("wheel", scrolling);
+}
+addScroll();
+
+window.addEventListener("load", () => {
+  window.scrollTo(0, 0);
 });
